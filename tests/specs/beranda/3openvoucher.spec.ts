64816eb843a0da2cf5b461732a3ddc278115f4ae
Tia Wicaksono <wicaksono.tia@gmail.com>
2023-06-02 10:02:13 +0700

    voucher

--- tests/specs/beranda/3openvoucher.spec.ts
+++ tests/specs/beranda/3openvoucher.spec.ts
@@ -1,16 +1,24 @@
 import { Given, When, Then } from '@cucumber/cucumber';
 import VoucherScreen from '../../screenobjects/beranda/voucherScreen';
 import BerandaScreen from '../../screenobjects/beranda/berandaScreen';
+import HiburanScreen from '../../screenobjects/beranda/hiburanScreen';
 
 Given(/^I am on the Home page$/, async () => {
     await driver.pause(5000)
+    console.log("masuk home");
 });
 
 When(/^I click Voucher$/, async () => {
-    await BerandaScreen.clickVoucher();
+    // await BerandaScreen.clickVoucher();
+    console.log("sudah tidak ada menu ini");
+    await BerandaScreen.clickHiburan()
+    await driver.pause(5000)
+    await HiburanScreen.back()
 });
 
 Then(/^I should be able to see my existing voucher$/, async () => {
+    // await driver.pause(5000)
+    // await VoucherScreen.openVoucher();
+    console.log("terima kasih");
     await driver.pause(5000)
-    await VoucherScreen.openVoucher();
 });
